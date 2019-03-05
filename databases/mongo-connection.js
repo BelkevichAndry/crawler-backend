1947fc91bc23841c189b5f73b2b403cd7ddf4cf5
BelkevichAndrew <belkevichandry@gmail.com>
2019-03-05 12:25:29 +0300

    d

--- databases/mongo-connection.js
+++ databases/mongo-connection.js
@@ -1,6 +1,6 @@
 'use strict';
 import mongoose from 'mongoose'
 
-const db =  mongoose.connect('mongodb://localhost:27017/local', {useNewUrlParser: true});
+const db =  mongoose.connect('mongodb://localhost:27017/crawler', {useNewUrlParser: true});
 
 export default db;
